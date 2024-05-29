8a6b64606056b57361231fa4790627b761652a20
Ángel Linero <116581341+Angelinero48@users.noreply.github.com>
2024-05-29 09:44:51 +0200

    v.01

---
+++ ServidorEscuchaActiva/ServidorEscuchaActiva/Program.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServidorEscuchaActiva
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (!File.Exists(Path.Combine(baseDir, "config.json")))
+            {
+                throw new FileNotFoundException("No se encontró el archivo config.json");
+            }
+
+            dynamic configObject = JsonConvert.DeserializeObject(File.ReadAllText(Path.Combine(baseDir, "config.json")));
+
+            int port = configObject.port;
+            bool deleteFiles = configObject.deleteFiles;
+            var files = configObject.files;
+
+            
+
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            try
+            {
+                listener.Start();
+                Console.WriteLine("Escuchando conexiones entrantes...");
+
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Conectado al cliente.");
+
+                    using (FileSystemWatcher watcher = new FileSystemWatcher())
+                    {
+
+                        foreach (var file in files)
+                        {
+                            string path = file.path;
+                            string pathCompleto;
+                            if (path.StartsWith("\\"))
+                            {
+                                path = path.TrimStart('\\');
+                                pathCompleto = Path.Combine(baseDir, path);
+                            }
+                            else
+                            {
+                                pathCompleto = path;
+                            }
+
+
+                            watcher.Path = pathCompleto;
+                            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+                            watcher.Filter = "*.*";
+                            watcher.EnableRaisingEvents = true;
+
+                            watcher.Created += async (sender, e) =>
+                            {
+                                foreach (var file in files)
+                                {
+                                    string path = file.path;
+                                    string prefix = file.prefix;
+                                    string ext = file.ext;
+
+                                    string pathCompleto;
+                                    if (path.StartsWith("\\"))
+                                    {
+                                        path = path.TrimStart('\\');
+                                        pathCompleto = Path.Combine(baseDir, path);
+                                    }
+                                    else
+                                    {
+                                        pathCompleto = path;
+                                    }
+
+                                    string[] archivos;
+                                    if (string.IsNullOrEmpty(prefix))
+                                    {
+                                        archivos = Directory.GetFiles(pathCompleto, "*.*", SearchOption.TopDirectoryOnly)
+                                            .Where(s => Path.GetExtension(s).Equals(ext, StringComparison.OrdinalIgnoreCase))
+                                            .ToArray();
+                                    }
+                                    else
+                                    {
+                                        archivos = Directory.GetFiles(pathCompleto, "*.*", SearchOption.TopDirectoryOnly)
+                                            .Where(s => Path.GetExtension(s).Equals(ext, StringComparison.OrdinalIgnoreCase) &&
+                                                        Path.GetFileName(s).StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                                            .ToArray();
+                                    }
+
+                                    foreach (var archivo in archivos)
+                                    {
+                                        Console.WriteLine("Enviando archivo: " + archivo);
+
+                                        byte[] archivoArray = File.ReadAllBytes(archivo);
+                                        string nombreConExtension = Path.GetFileName(archivo);
+                                        byte[] nombreBytes = Encoding.UTF8.GetBytes(nombreConExtension);
+                                        byte[] longitudNombreBytes = BitConverter.GetBytes(nombreBytes.Length);
+
+                                        try
+                                        {
+                                            if (client.Connected)
+                                            {
+                                                NetworkStream ns = client.GetStream();
+
+                                                // Enviar la longitud del nombre del archivo
+                                                await ns.WriteAsync(longitudNombreBytes, 0, longitudNombreBytes.Length);
+
+                                                // Enviar el nombre del archivo
+                                                await ns.WriteAsync(nombreBytes, 0, nombreBytes.Length);
+
+                                                // Enviar los datos del archivo
+                                                await ns.WriteAsync(archivoArray, 0, archivoArray.Length);
+
+                                                Console.WriteLine($"{nombreConExtension} enviado al cliente");
+
+                                                await Task.Delay(1000);
+
+                                                if (deleteFiles)
+                                                {
+                                                    File.Delete(archivo);
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("El cliente se ha desconectado.");
+                                                break; // Salir del bucle si el cliente se desconecta
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Console.WriteLine($"Error al enviar archivo: {ex.Message}");
+                                            await Task.Delay(TimeSpan.FromSeconds(1)); // Espera antes de reintentar
+                                        }
+                                    }
+                                }
+
+                            };
+
+                            // Mantener la aplicación corriendo
+                            await Task.Delay(Timeout.Infinite);
+                        }
+                    }
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ clienteEscuchaActiva/clienteEscuchaActiva/Program.cs
@@ -0,0 +1,158 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using IronPdf;
+using System.Linq.Expressions;
+
+namespace ClienteEscuchaActiva
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (!File.Exists(Path.Combine(baseDir, "config.json")))
+            {
+                throw new FileNotFoundException("No se encontró el archivo config.json");
+            }
+            else
+            {
+                try
+                {
+                    dynamic configObject = JsonConvert.DeserializeObject(File.ReadAllText(Path.Combine(baseDir, "config.json")));
+
+                    int port = configObject.port;
+                    string ip = configObject.ip;
+                    string path = configObject.path;
+                    string accion = configObject.accion;
+                    var printers = configObject.printers;
+                    bool deleteFiles = configObject.deleteFiles;
+
+                    while (true)
+                    {
+                        using (TcpClient cliente = new TcpClient())
+                        {
+                            try
+                            {
+                                await cliente.ConnectAsync(ip, port);
+                                Console.WriteLine("Conectado al servidor.");
+
+                                var stream = cliente.GetStream();
+
+                                while (cliente.Connected)
+                                {   
+                                        byte[] longitudNombreBytes = new byte[sizeof(int)];
+                                        int bytesRead = await stream.ReadAsync(longitudNombreBytes, 0, longitudNombreBytes.Length);
+
+                                        int longitudNombre = BitConverter.ToInt32(longitudNombreBytes, 0);
+
+                                        byte[] nombreBytes = new byte[longitudNombre];
+                                        bytesRead = await stream.ReadAsync(nombreBytes, 0, nombreBytes.Length);
+
+
+                                        string nombreArchivo = Encoding.UTF8.GetString(nombreBytes);
+
+
+                                        using (MemoryStream memoryStream = new MemoryStream())
+                                        {
+                                            byte[] buffer = new byte[1024];
+                                            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                                            {
+                                                await memoryStream.WriteAsync(buffer, 0, bytesRead);
+                                                if (bytesRead < buffer.Length)
+                                                {
+                                                    break;
+                                                }
+                                            }
+                                            
+                                            byte[] pdfBytes = memoryStream.ToArray();
+
+                                            string pdfSavePath = Path.Combine(baseDir, path, $"{nombreArchivo}");
+
+                                            Directory.CreateDirectory(Path.Combine(baseDir, path));
+
+                                            File.WriteAllBytes(pdfSavePath, pdfBytes);
+
+
+                                            string tipoArchivo = nombreArchivo.Substring(0, 3);
+
+                                            switch (accion)
+                                            {
+                                                case "open":
+                                                    Process.Start(new ProcessStartInfo
+                                                    {
+                                                        FileName = pdfSavePath,
+                                                        UseShellExecute = true
+                                                    });
+                                                    break;
+
+                                                case "print":
+                                                    foreach (var impresiones in printers)
+                                                    {
+                                                        if (tipoArchivo == impresiones.type.ToString())
+                                                        {
+                                                            string impresora = impresiones.printer;
+                                                            short quantity = impresiones.quantity;
+                                                            bool duplex = impresiones.duplex;
+
+                                                            var networkPrinterName = impresora;
+                                                            PdfDocument pdf = new PdfDocument(pdfSavePath);
+                                                            PrinterSettings settings = new PrinterSettings()
+                                                            {
+                                                                PrinterName = networkPrinterName,
+                                                                Copies = quantity,
+                                                                Duplex = duplex ? Duplex.Horizontal : Duplex.Simplex,
+                                                            };
+                                                            PrintDocument document = pdf.GetPrintDocument(settings);
+
+                                                            foreach (string printer2 in PrinterSettings.InstalledPrinters)
+                                                            {
+                                                                if (printer2 == networkPrinterName)
+                                                                {
+                                                                    document.Print();
+                                                                    Console.WriteLine($"{nombreArchivo} enviado a imprimir. \nImpresora: {impresora}\nNº copias: {quantity}");
+                                                                    break;
+                                                                }
+                                                            }
+
+                                                            if (deleteFiles)
+                                                            {
+                                                                File.Delete(pdfSavePath);
+                                                            }
+                                                        }
+                                                    }
+                                                    break;
+                                            }
+                                        }
+                                    
+                                }
+                            }
+                            catch (IOException ex)
+                            {
+                                Console.WriteLine($"Error de E/S: {ex.Message}");
+                            }
+                            catch (SocketException ex)
+                            {
+                                Console.WriteLine($"Error de socket: {ex.Message}");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"Error: {ex.Message}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
